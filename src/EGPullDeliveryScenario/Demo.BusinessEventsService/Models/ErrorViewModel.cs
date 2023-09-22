60cc20699f7588588bb87eae180684b29d6d1280
Abbas N <anandwani@microsoft.com>
2023-09-22 23:22:46 +0100

    pushing solution

---
+++ src/EGPullDeliveryScenario/Demo.BusinessEventsService/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Demo.BusinessEventsService.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ src/EGPullDeliveryScenario/Demo.BusinessEventsService/Services/TransactionService.cs
@@ -0,0 +1,40 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using TransactionEvent;
+
+namespace Demo.BusinessEventsService.Services
+{
+    public class TransactionService : TransactionEvents.TransactionEventsBase
+    {
+        private readonly ILogger<TransactionService> _logger;
+        public TransactionService(ILogger<TransactionService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override async Task GetTransactionStream(Empty _, IServerStreamWriter<TransactionEventData> responseStream, 
+            ServerCallContext context)
+        {
+            var rng = new Random();
+            var now = DateTime.UtcNow;
+
+            var i = 0;
+            while (!context.CancellationToken.IsCancellationRequested && i < 20)
+            {
+                await Task.Delay(500); // Gotta look busy
+
+                var transaction = new TransactionEventData
+                {
+                    TransactionDateTime = Timestamp.FromDateTime(now.AddDays(i++)),
+                    Transactionid = Guid.NewGuid().ToString(),
+                    Clientid = Guid.NewGuid().ToString(),
+                    Amount = rng.Next(20, 1550)
+                };
+
+                _logger.LogInformation("Sending AccountTransactionData response");
+
+                await responseStream.WriteAsync(transaction);
+            }
+        }
+    }
+}
