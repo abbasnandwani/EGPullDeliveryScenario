286c7237e0ad246ab9ce4febedb64e7fe04ae327
Abbas N <anandwani@microsoft.com>
2023-09-24 22:41:29 +0100

    checkin 24 Sep

--- src/EGPullDeliveryScenario/Demo.Consumer/Functions.cs
+++ src/EGPullDeliveryScenario/Demo.Consumer/Functions.cs
@@ -10,6 +10,10 @@ using System.Threading;
 using System.Threading.Tasks;
 using Azure.Storage.Blobs;
 using Newtonsoft.Json;
+using Azure.Messaging.EventGrid;
+using Azure.Messaging.EventGrid.Namespaces;
+using Azure;
+using Azure.Messaging;
 
 namespace Demo.Consumer
 {
@@ -20,7 +24,7 @@ namespace Demo.Consumer
             [Blob("eventfiles", Connection = "BLOB_CONNSTR")] BlobContainerClient blobContainerClient,
             ILogger log)
         {
-            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+            log.LogInformation($"Executing function to fetch events at: {DateTime.Now}");
 
 
             string serviceApiEndPoint = Environment.GetEnvironmentVariable("SERVICE_API_ENDPOINT");
@@ -37,10 +41,16 @@ namespace Demo.Consumer
             //create blob container if does not exists
             await blobContainerClient.CreateIfNotExistsAsync();
 
+            //create evetn grid client
+            EventGridClient egClient = new EventGridClient(new Uri(topicEndpoint), new AzureKeyCredential(topicKey));
+                        
+            //create event request setting
             TransactionEvent.EventRequestSetting requestSetting = new TransactionEvent.EventRequestSetting
             {
                 MaxEvents = 2
             };
+
+            log.LogInformation($"Fetching max {requestSetting.MaxEvents} events");
 
             using var streamingCall = client.GetTransactionStream(requestSetting, cancellationToken: cts.Token);
 
@@ -56,12 +66,29 @@ namespace Demo.Consumer
                     var blobClient = blobContainerClient.GetBlobClient(filename);
 
                     await blobClient.UploadAsync(BinaryData.FromString(jsonPayload), overwrite: true);
+
+                    //publish event in event grid
+                    EventInfo info = new EventInfo();
+
+                    info.FileName = filename;
+                    info.TransactionId = transactionEventData.Transactionid;
+
+                    CloudEvent cloudEvent = new CloudEvent("Demo.BusinessEvent", "Business.Event", info);
+                    await egClient.PublishCloudEventAsync(topicName, cloudEvent);
                 }
             }
             catch (RpcException ex) when (ex.StatusCode == StatusCode.Cancelled)
             {
                 log.LogError("Stream cancelled.");
             }
+
+            log.LogInformation($"Fetching events complete.");
         }
+    }
+
+    public class EventInfo
+    {
+        public string TransactionId { get; set; }
+        public string FileName { get; set; }
     }
 }
